13fe42a1264fb17749014e8a03138c3ab23db30c
syedvaiz <vaizmehmoodi@gmail.com>
2019-07-21 18:13:49 +0530

    first commit

--- src/components/NewsListitem.js
+++ src/components/NewsListitem.js
@@ -2,19 +2,21 @@ import React from 'react';
 import './styles/newsListStyle.css';
 const NewsListitem = () => {
     return(
+        <div className='parentContainer'>
         <div className='mainContainer'>
             <img src="http://tineye.com/images/widgets/mona.jpg"
             alt=""
             width="150px"
             height="125" />
-            <div className='newsTitle'>
-                <p className='textWrapper'>
+            <div>
+                <p className='newsTitle'>
                     Water World
                 </p>
                 <p className='textWrapper'>
                     KSDJFKJHPEIUFGHVNCKXVN; K JHFKLDHKLJ GUDFHGKJDHLFGKJDFH OIHJLK;DFHG 
                 </p>
             </div>
+        </div>
         </div>
     );
 }
